7c26405f1f9546aa434acf4f12ada47284223d61
souravnarula <promatics.souravnarula@gmail.com>
2021-07-12 11:15:54 +0530

    comments added

--- Dreesha User/Controllers/Album Details/AlbumDetailsViewController.swift
+++ Dreesha User/Controllers/Album Details/AlbumDetailsViewController.swift
@@ -50,6 +50,10 @@ class AlbumDetailsViewController: UIViewController ,UICollectionViewDelegate , U
      Now the change is edited from abhishek's end.
      */
     
+    /**
+     Check it now again.
+     */
+    
     //MARK: View Will Appear
     override func viewWillAppear(_ animated: Bool) {
         setBackWithTitle(titleVal : "Album Details")
